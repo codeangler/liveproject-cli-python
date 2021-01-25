67fc7c990946cc68c2b8ab893567315f86455977
Casey Burnett <burnett.casey@gmail.com>
2021-01-25 08:04:00 -0700

    test(cli): unit test and integration test layout

--- tests/test_klickbrick.py
+++ tests/test_klickbrick.py
@@ -1,5 +1,10 @@
 from klickbrick import __version__
-
+import unittest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+class TestStringMethod(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
